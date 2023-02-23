bb06fe214cbe7d505f663b62d25bc01bc4a51bfd
GalaxyGamerYT <OliWootton@Outlook.com>
2023-02-23 19:22:59 +0000

    fixed bugs and included python above 3.

--- src/galaxy_encrytion/galaxyEncryption.py
+++ src/galaxy_encrytion/galaxyEncryption.py
@@ -4,9 +4,12 @@ from cryptography.fernet import Fernet
 
 print("Thank you for using Galaxy Encryption")
 
-class Encryption:
+class galaxyEncryption:
     fernet = None
     def key(self):
+        """
+        Generates the key and/or loads the key.
+        """
         if not path.exists("key.key"):
             # key generation
             key = Fernet.generate_key()
@@ -14,15 +17,20 @@ class Encryption:
             # string the key in a file
             with open('key.key', 'wb') as filekey:
                 filekey.write(key)
+            
         else:
             # opening the key
             with open('filekey.key', 'rb') as filekey:
                 key = filekey.read()
             
-            # using the key
-            self.fernet = Fernet(key)
+        # using the key
+        self.fernet = Fernet(key)
 
     def decryptFile(self, path: str) -> str:
+        """
+        Decrypts the given contents of the file path.
+        Needs the key function to have run.
+        """
         if self.fernet != None:
             # opening the encrypted file
             with open(path, 'rb') as file:
@@ -35,6 +43,10 @@ class Encryption:
             raise Exception("You need to generate/load a key using the key method of this module.")
     
     def encryptFile(self, path: str, content: str) -> str:
+        """
+        Encrypts the given content and writes to a file.
+        Needs the key function to have run.
+        """
         if self.fernet != None:
             encrypted = self.fernet.encrypt(content)
     
